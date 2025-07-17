caf3bde9679e2ecec97d57c6d843fd864f6f2f0f
Shafiq Shams <shafiqshamsessani@gmail.com>
2025-07-18 01:49:07 +0200

    feat: add basic ui and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { useState } from 'react'
 import './App.css'
+
+const initialBoard = () => Array(9).fill(null);
 
 function App() {
 
+  const [board, setBoard] = useState(initialBoard);
+  console.log(board);
+
   return (
-    <h1>Hello World</h1>
+    <div className="game">
+      <div className="status">
+        Player X Turn
+        <button className='reset-button'>Reset Game</button>
+      </div>
+
+      <div className="board">
+        {board.map((_, index) => {
+          return <button className="cell" key={`cell-${index}`}>X</button>
+        })}
+      </div>
+    </div>
+
   )
 }
 
