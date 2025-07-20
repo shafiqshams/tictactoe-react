4b7bbde5dd6732c3b1f50abe68ba559e33d716c7
Shafiq Shams <shafiqshamsessani@gmail.com>
2025-07-20 21:17:31 +0200

    feat(utils): add board helper methods

---
+++ src/utils/boardHelpers.ts
@@ -0,0 +1,30 @@
+import type { Board, Column, Row } from "../types/board.types"
+
+export const getInitialBoard = (boardSize: number): Board => {
+   return Array(boardSize)
+      .fill(null)
+      .map(() => Array(boardSize).fill(null));
+}
+
+export const getRow = (board: Board, rowIndex: number): Row => {
+   return board[rowIndex];
+}
+
+export const getColumn = (board: Board, columnIndex: number): Column => {
+   return board.map(row => row[columnIndex])
+}
+
+export const getDiagonals = (board: Board): [Row, Column] => {
+   const primaryDiagonal = [], secondaryDiagonal = [];
+
+   for (let i = 0; i < board.length; i++) {
+      primaryDiagonal[i] = board[i][i];
+      secondaryDiagonal[i] = board[i][2 - i];
+   }
+
+   return [primaryDiagonal, secondaryDiagonal];
+}
+
+export const isBoardFull = (board: Board): boolean => {
+   return board.every(row => row.every(cell => cell !== null))
+}
