7caea7933941812cb24d96256ef3b97095cf6547
Shafiq Shams <shafiqshamsessani@gmail.com>
2025-07-22 01:33:53 +0200

    refactor: update function parameter

--- src/utils/boardHelpers.ts
+++ src/utils/boardHelpers.ts
@@ -18,8 +18,8 @@ export const getPrimaryDiagonal = (board: Board, rowIndex: number, colIndex: num
    return rowIndex === colIndex ? board.map((row, i) => row[i]) : []
 }
 
-export const getSecondaryDiagonal = (board: Board, rowIndex: number, colIndex: number, boardSize: number): Cell[] => {
-   return rowIndex + colIndex === boardSize - 1 ? board.map((row, i) => row[boardSize - (i + 1)]) : []
+export const getSecondaryDiagonal = (board: Board, rowIndex: number, colIndex: number): Cell[] => {
+   return rowIndex + colIndex === board.length - 1 ? board.map((row, i) => row[board.length - (i + 1)]) : []
 }
 
 export const isBoardFull = (board: Board): boolean => {
